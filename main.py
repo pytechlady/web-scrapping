1a785a95b7a232d087b273da7df04b0dce32623a
newbietechlady <fumex9910@gmail.com>
2021-11-10 13:17:31 -0800

    web scraper

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 # An entrypoint of your application
+import requests.exceptions
+from plot import PlotGraph
+
+
+draw_bar = PlotGraph()
+while True:
+    cont_checking = input("Would you like to scrap a site? 'y'/'n': ").lower()
+    if cont_checking == "y":
+        try:
+            draw_bar.make_graph()
+        except requests.exceptions.MissingSchema:
+            print("Invalid URL")
+        except requests.exceptions.ConnectTimeout:
+            print("404 error loading page")
+        except requests.exceptions.ConnectionError:
+            print("Connection error. Please try again later")
+
+    elif cont_checking == "n":
+        print("Thanks for analyzing! Come back again!")
+        break
+    else:
+        print("Please enter y or n")
---
+++ plot.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+from request import RequestUrl
+
+
+class PlotGraph:
+    # function to plot the pie chart
+    def make_graph(self):
+        new_word = []
+        res = RequestUrl()
+        for item in res.count_occur():
+            new_word.append(item)
+        print(f'The top word is: {new_word[0]}')
+        x = res.count_occur().keys()
+        y = res.count_occur().values()
+        plt.pie(y, labels=x, autopct="%1.1f%%")
+        plt.show()
